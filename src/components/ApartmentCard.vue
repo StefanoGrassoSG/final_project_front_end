2c4b4b0bb0234ef1b3d08193dc00aab99ca044c8
aalexebr <alex.ebr@icloud.com>
2023-10-18 00:03:06 +0200

    ADD v-if on image

--- src/components/ApartmentCard.vue
+++ src/components/ApartmentCard.vue
@@ -30,7 +30,8 @@
             address: {{ address }}
         </h4>
         <div class="img">
-            <img :src="path" alt="" class="img-fluid">
+            <img :src="'http://127.0.0.1:8000/storage/'+path" alt="" class="img-fluid" v-if="path.startsWith('uploads')">
+            <img :src="path" alt="" v-if="!path.startsWith('uploads')" class="img-fluid">
         </div>
     </div>
 </template>
