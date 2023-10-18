0f5478ad580fb44a1de5b5feedf5bcf89c946924
aalexebr <alex.ebr@icloud.com>
2023-10-18 08:31:07 +0200

    hotfix add loading and v-if con cards

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,19 +2,26 @@
     import axios from "axios";
     import BannerComponent from '../components/BannerComponent.vue';
     import ApartmentCard from '../components/ApartmentCard.vue';
+    import LoadingComponent from '../components/LoadingComponent.vue';
   export default {
   data() {
     return {
-        apartments : null
+        apartments : null,
+        aptLoading :false
     }
   },
   methods: {
     getApt(){
+		this.aptLoading = true
         axios.get('http://127.0.0.1:8000/api/apartment')
             .then(res=>{
                 console.log(res.data.results.data)
                 this.apartments = res.data.results.data
+				this.aptLoading = false
             })
+			.catch(err=>{
+				console.log(err)
+			})
     }
   },
   created(){
@@ -22,7 +29,8 @@
   },
   components: {
     BannerComponent,
-    ApartmentCard
+    ApartmentCard,
+    LoadingComponent
   }
 }
 </script>
@@ -34,12 +42,22 @@
         </h1>
     <div class="container">
         <div class="d-flex justify-content-between flex-wrap align-content-stretch w-100">
-            <div class="col-4 my-3 px-2 d-flex align-content-stretch" v-for="(apt,i) in apartments" :key="i">
-                    <ApartmentCard
-                    :name="apt.name"
-                    :address="apt.address"
-                    :path="apt.cover_img"/>
-            </div>
+			<template v-if="aptLoading">
+				<LoadingComponent/>
+			</template>
+           <template v-if="!aptLoading">
+				<template v-if="apartments.length > 0 || apartments.lenght != null">
+					<div class="col-4 my-3 px-2 d-flex align-content-stretch" v-for="(apt,i) in apartments" :key="i">
+						<ApartmentCard
+						:name="apt.name"
+						:address="apt.address"
+						:path="apt.cover_img"/>
+					</div>
+				</template>
+				<template v-else>
+					no results found
+				</template>
+		   </template>
         </div>
     </div>
 </template>
