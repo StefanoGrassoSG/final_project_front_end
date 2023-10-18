fb8f4adde4909dfb494fa731c624ed84892450f6
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-10-18 21:22:28 +0200

    FIXED homepage

--- src/components/ApartmentCard.vue
+++ src/components/ApartmentCard.vue
@@ -1,41 +1,74 @@
 <script>
-    import axios from "axios";
+import axios from "axios";
 
-  export default {
-  data() {
-    return {
-        apartments : null
+export default {
+    data() {
+        return {
+            apartments: null
+        }
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+    props: {
+        name: String,
+        address: String,
+        path: String
     }
-  },
-  methods: {
-
-  },
-  created(){
-
-  },
-  props:{
-    name: String,
-    address: String,
-    path: String
-  }
 }
 </script>
 
 <template>
-    <div class="card">
-        <h3>
-            {{ name }}
-        </h3>
-        <h4>
-            address: {{ address }}
-        </h4>
-        <div class="img">
-            <img :src="'http://127.0.0.1:8000/storage/'+path" alt="" class="img-fluid" v-if="path.startsWith('uploads')">
+    <div class="card border rounded-4">
+        <div class="img border rounded-4">
+            <img :src="'http://127.0.0.1:8000/storage/' + path" alt="" class="img-fluid" v-if="path.startsWith('uploads')">
             <img :src="path" alt="" v-if="!path.startsWith('uploads')" class="img-fluid">
         </div>
+        <div class="info-apt m-3">
+            <h4>
+                {{ name }}
+            </h4>
+            <div>
+                <span class="info-small-apt">
+                    Indirizzo:
+                </span>
+                {{ address }}
+            </div>
+            <div>
+                <span class="info-small-apt">
+                    Prezzo:
+                </span>
+                {{ price }} X&euro;/notte
+            </div>
+            <div>
+                <span class="info-small-apt">
+                    Mq:
+                </span>
+            </div>
+        </div>
+
     </div>
 </template>
 
-<style lang="scss" scoped>;
+<style lang="scss" scoped>
+img {
+    border-start-end-radius: 10px;
+    border-start-start-radius: 10px;
+}
 
+.info-apt {
+    h4 {
+        font-weight: bold;
+        color: #3461AB;
+    }
+}
+
+.info-small-apt {
+    font-size: 15px;
+    font-weight: bold;
+    color: #888A8C;
+}
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,27 +4,27 @@ import BannerComponent from '../components/BannerComponent.vue';
 import ApartmentCard from '../components/ApartmentCard.vue';
 import LoadingComponent from '../components/LoadingComponent.vue';
 
-  export default {
-  data() {
-    return {
-        apartments : null,
-		totalAptPages : null,
-		nextPageCounter : 1,
-        aptLoading :false,
-		inputSearchbar: null
-    }
-  },
-  mounted() {
+export default {
+	data() {
+		return {
+			apartments: null,
+			totalAptPages: null,
+			nextPageCounter: 1,
+			aptLoading: false,
+			inputSearchbar: null
+		}
+	},
+	mounted() {
 		var options = {
 			minNumberOfCharacters: 0,
 			searchOptions: {
-			key: "KEiNGuhsySt5PgvkmCw7C9Sb5vGdacR6",
-			language: "it-IT",
-			limit: 5,
+				key: "KEiNGuhsySt5PgvkmCw7C9Sb5vGdacR6",
+				language: "it-IT",
+				limit: 5,
 			},
 			autocompleteOptions: {
-			key: "KEiNGuhsySt5PgvkmCw7C9Sb5vGdacR6",
-			language: "it-IT",
+				key: "KEiNGuhsySt5PgvkmCw7C9Sb5vGdacR6",
+				language: "it-IT",
 			},
 		}
 		var ttSearchBox = new tt.plugins.SearchBox(tt.services, options)
@@ -35,117 +35,116 @@ import LoadingComponent from '../components/LoadingComponent.vue';
 		ttSearchBox.on("tomtom.searchbox.resultsfound", this.handleResultsFound)
 
 	},
-  methods: {
-	handleResultsFound(event) {
-        var results = event.data.results;
-		if(event.data.results.fuzzySearch.results.length === 0 ){
-			this.getApt()
-		}
-		else{
-			this.search(results.autocomplete.context.inputQuery);
-		}
-        
-    },
-    getApt(){
-		this.aptLoading = true
-        axios.get('http://127.0.0.1:8000/api/apartment')
-            .then(res=>{
-                this.apartments = res.data.results.data
-				this.totalAptPages = res.data.results.last_page
-				this.aptLoading = false
-            })
-			.catch(err=>{
-				console.log(err)
-				this.aptLoading = false
-			})
-    },
-	search(param) {
-		this.aptLoading = true
-		let data = {
-			data: param
-		}
-		axios.post('http://127.0.0.1:8000/api/apartment', data, {
-			headers: {
-				'Content-Type': 'multipart/form-data'
+	methods: {
+		handleResultsFound(event) {
+			var results = event.data.results;
+			if (event.data.results.fuzzySearch.results.length === 0) {
+				this.getApt()
 			}
-		})
-		.then(res => {
-			console.log(res)
-			this.apartments = res.data.results.data
-			this.totalAptPages = res.data.results.last_page
-			this.aptLoading = false
-		})
-		.catch(err => {
-			console.log(err)
-		})
-	},
-	nextPage(){
-		if(this.nextPageCounter < this.totalAptPages){
-			this.nextPageCounter++
-		}
-		else{
-			this.nextPageCounter = 1
-		}
-		this.aptLoading = true
-		axios.get(`http://127.0.0.1:8000/api/apartment?page=${this.nextPageCounter}`)
-		.then(res=>{
-                console.log(res.data.results)
-                this.apartments = res.data.results.data
-				this.aptLoading = false
-            })
-			.catch(err=>{
-				console.log(err)
-				this.aptLoading = false
+			else {
+				this.search(results.autocomplete.context.inputQuery);
+			}
+
+		},
+		getApt() {
+			this.aptLoading = true
+			axios.get('http://127.0.0.1:8000/api/apartment')
+				.then(res => {
+					this.apartments = res.data.results.data
+					this.totalAptPages = res.data.results.last_page
+					this.aptLoading = false
+				})
+				.catch(err => {
+					console.log(err)
+					this.aptLoading = false
+				})
+		},
+		search(param) {
+			this.aptLoading = true
+			let data = {
+				data: param
+			}
+			axios.post('http://127.0.0.1:8000/api/apartment', data, {
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				}
 			})
-	}
-  },
-  created(){
+				.then(res => {
+					console.log(res)
+					this.apartments = res.data.results.data
+					this.totalAptPages = res.data.results.last_page
+					this.aptLoading = false
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		},
+		nextPage() {
+			if (this.nextPageCounter < this.totalAptPages) {
+				this.nextPageCounter++
+			}
+			else {
+				this.nextPageCounter = 1
+			}
+			this.aptLoading = true
+			axios.get(`http://127.0.0.1:8000/api/apartment?page=${this.nextPageCounter}`)
+				.then(res => {
+					console.log(res.data.results)
+					this.apartments = res.data.results.data
+					this.aptLoading = false
+				})
+				.catch(err => {
+					console.log(err)
+					this.aptLoading = false
+				})
+		}
+	},
+	created() {
 		this.getApt();
-  },
-  components: {
-    BannerComponent,
-    ApartmentCard,
-    LoadingComponent
-  }
+	},
+	components: {
+		BannerComponent,
+		ApartmentCard,
+		LoadingComponent
+	}
 }
 </script>
 
 <template>
-    <BannerComponent/>
-        <h1 class="text-center my-5"> 
-            QUESTO è IL CONTENUTO DELLA HOME PAGE
-        </h1>
+	<BannerComponent />
 
-		<div ref="prova" @keyup.enter="search()" class="w-25">
-	
+	<div class="search-bar-div">
+		<div ref="prova" @keyup.enter="search()" class="w-50">
+
 		</div>
+	</div>
 
-		
 
-    <div class="container">
-        <div class="d-flex justify-content-between flex-wrap align-content-stretch w-100">
+
+
+	<div class="container">
+		<div id="my-container" class="mt-5 d-flex justify-content-center flex-wrap align-content-stretch w-100">
 			<template v-if="aptLoading">
-				<LoadingComponent/>
+				<LoadingComponent />
 			</template>
-           <template v-if="!aptLoading">
+			<template v-if="!aptLoading">
 				<template v-if="apartments.length > 0 || apartments.lenght != null">
-					<div class="col-4 my-3 px-2 d-flex align-content-stretch" v-for="(apt,i) in apartments" :key="i">
-						<ApartmentCard
-						:name="apt.name"
-						:address="apt.address"
-						:path="apt.cover_img"/>
+					<div class="col-4 my-3 px-2 d-flex align-content-stretch" v-for="(apt, i) in apartments" :key="i">
+						<ApartmentCard :name="apt.name" :address="apt.address" :path="apt.cover_img" />
 					</div>
 				</template>
 				<template v-else>
 					no results found
 				</template>
-				<button @click="nextPage" v-if="totalAptPages != nextPageCounter"
-					class="btn btn-success">next</button>
-		   </template>
-        </div>
-    </div>
+				<button @click="nextPage" v-if="totalAptPages != nextPageCounter" class="btn btn-success">next</button>
+			</template>
+		</div>
+	</div>
 </template>
 
-<style lang="scss" scoped>;
-
+<style lang="scss" scoped>
+.search-bar-div {
+	display: flex;
+	justify-content: center;
+}
 </style>
